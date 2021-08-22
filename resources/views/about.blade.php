3e931c3a10e1afbd294e77a148eac568296d00ed
tomoya-matsuo <musiclove_drummer@ezweb.ne.jp>
2021-08-22 10:45:06 +0000

    top page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center pr-1 pt-5">
+        <h1 class="display-5">自分の趣味は、どれ？</h1>
+        <h4 class="pt-3">「自分にはこれといった趣味が無い...。新しい趣味に挑戦してみたい...!」</h4>
+        <h5 class="pt-5">そう思う方々のために、すでに趣味を持っている方々が自分の趣味を共有し、<br>「自分の趣味は、これ！」というものを見つけることを趣旨としたサービスです。</h5>
+        <h5 class="pt-3">もちろん既に趣味を持たれている方々も、知らなかった趣味や前から<br>興味があったけどなかなか出来なかった趣味等を見つけることが出来ます！</h5>
+        <h2 class="pt-5">さあ、新しい趣味・自分を見つけよう！</h2>
+        <img src="{{ '/img/about.png' }}" class="img-fluid">
+    </div>
+@endsection
---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,22 @@
+<header class="mb-5">
+    <nav class="navbar navbar-expand-sm navbar-light">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand text-light" href="/">シュミコレ</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{--アバウトページへのリンク--}}
+                <li class="nav-item"><a href="#" class="nav-link text-light">シュミコレとは?</a></li>
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link text-light">新規登録</a></li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link text-light">ログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
